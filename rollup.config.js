7257a40575f2b14249fbdba9db19dd42bca46432
Jacob Buck <buck.jacob@gmail.com>
2020-07-10 12:36:32 +1200

    Update rollup.config.js
    Set babelHelpers config on @rollup/plugin-babel

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,9 @@ import babel from '@rollup/plugin-babel';
 export default {
   input: 'src/index.js',
   output: [
-    {
-      file: 'lib/index.cjs.js',
-      format: 'cjs',
-    },
-    {
-      file: 'lib/index.esm.js',
-      format: 'esm',
-    },
+    { file: 'lib/index.cjs.js', format: 'cjs' },
+    { file: 'lib/index.esm.js', format: 'esm' },
   ],
   external: ['prop-types', 'react', 'use-latest'],
-  plugins: [babel()],
+  plugins: [babel({ babelHelpers: 'bundled' })],
 };
