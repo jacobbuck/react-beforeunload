a7c2a52adc406dcff4d3bb718960fdc4687bcfe5
Jacob Buck <buck.jacob@gmail.com>
2020-08-28 09:24:38 +0000

    Add useBeforeunload.test.js

---
+++ src/__tests__/useBeforeunload.test.js
@@ -0,0 +1,53 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useBeforeunload from '../useBeforeunload';
+
+const createBeforeunloadEvent = () =>
+  new Event('beforeunload', { cancelable: true });
+
+const renderUseBeforeunloadHook = (handler) =>
+  renderHook(() => useBeforeunload(handler));
+
+test('uses default handler when not set', () => {
+  const { result } = renderUseBeforeunloadHook();
+  window.dispatchEvent(createBeforeunloadEvent());
+  expect(result.error).toBeUndefined();
+});
+
+test('throws TypeError when handler is not a function', () => {
+  const { result } = renderUseBeforeunloadHook({ hello: 'world' });
+  expect(result.error).toEqual(
+    TypeError('Expected "handler" to be a function, not object.')
+  );
+});
+
+test('handler function is called when beforeunload event is fired', () => {
+  const handler = jest.fn();
+  renderUseBeforeunloadHook(handler);
+  const event = createBeforeunloadEvent();
+  window.dispatchEvent(event);
+  expect(handler).toHaveBeenCalledWith(event);
+});
+
+test('returnValue on event is set when preventDefault is called', () => {
+  renderUseBeforeunloadHook((event) => {
+    event.preventDefault();
+  });
+  const event = createBeforeunloadEvent();
+  // jsdom currently doesn't have `BeforeUnloadEvent` implemented, so we're just
+  // ensuring `returnValue` is set on `event`
+  const set = jest.fn();
+  Object.defineProperty(event, 'returnValue', { set });
+  window.dispatchEvent(event);
+  expect(set).toHaveBeenCalledWith('');
+});
+
+test('returnValue on event is set when a string is returned by handler', () => {
+  renderUseBeforeunloadHook(() => 'goodbye');
+  const event = createBeforeunloadEvent();
+  // jsdom currently doesn't have `BeforeUnloadEvent` implemented, so we're just
+  // ensuring `returnValue` is set on `event`
+  const set = jest.fn();
+  Object.defineProperty(event, 'returnValue', { set });
+  window.dispatchEvent(event);
+  expect(set).toHaveBeenCalledWith('goodbye');
+});
