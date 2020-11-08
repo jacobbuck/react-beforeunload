ceafcae553f1b67e7e8425cf195eab82423497ef
Jacob Buck <buck.jacob@gmail.com>
2020-11-08 05:07:35 +0000

    Generate source maps from rollup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## Unreleased
+
+### Added
+
+- Added source maps to build output.
+
 ## v2.3.0 - 2020-10-26
 
 ### Changed
--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,8 @@ import babel from '@rollup/plugin-babel';
 export default {
   input: 'src/index.js',
   output: [
-    { file: 'lib/index.cjs.js', format: 'cjs' },
-    { file: 'lib/index.esm.js', format: 'esm' },
+    { file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true },
+    { file: 'lib/index.esm.js', format: 'esm', sourcemap: true },
   ],
   external: ['prop-types', 'react', 'use-latest'],
   plugins: [babel({ babelHelpers: 'bundled' })],
