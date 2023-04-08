4d9ababb3ed9ca4902853fa7b593b0e687d404f5
Jacob Buck <buck.jacob@gmail.com>
2023-04-08 03:46:31 +0000

    Add conditional listening to useBeforeunload hook

--- src/useBeforeunload.js
+++ src/useBeforeunload.js
@@ -1,38 +1,33 @@
 import { useEffect, useRef } from 'react';
-import invariant from 'tiny-invariant';
 
 const useBeforeunload = (handler) => {
-  invariant(
-    handler == null || typeof handler === 'function',
-    'Expected `handler` to be a function'
-  );
-
-  const eventListenerRef = useRef();
-
+  const handlerRef = useRef(handler);
   useEffect(() => {
-    eventListenerRef.current = (event) => {
-      const returnValue = handler?.(event);
-      // Handle legacy `event.returnValue` property
-      // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event
-      if (typeof returnValue === 'string') {
-        return (event.returnValue = returnValue);
-      }
-      // Chrome doesn't support `event.preventDefault()` on `BeforeUnloadEvent`,
-      // instead it requires `event.returnValue` to be set
-      // https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeunload#browser_compatibility
-      if (event.defaultPrevented) {
-        return (event.returnValue = '');
-      }
-    };
-  }, [handler]);
-
+    handlerRef.current = handler;
+  });
+  const hasHandler = typeof handler === 'function';
   useEffect(() => {
-    const eventListener = (event) => eventListenerRef.current(event);
-    window.addEventListener('beforeunload', eventListener);
-    return () => {
-      window.removeEventListener('beforeunload', eventListener);
-    };
-  }, []);
+    if (hasHandler) {
+      const listener = (event) => {
+        const returnValue = handlerRef.current(event);
+        // Handle legacy `event.returnValue` property
+        // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event
+        if (typeof returnValue === 'string') {
+          return (event.returnValue = returnValue);
+        }
+        // Chrome doesn't support `event.preventDefault()` on `BeforeUnloadEvent`,
+        // instead it requires `event.returnValue` to be set
+        // https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeunload#browser_compatibility
+        if (event.defaultPrevented) {
+          return (event.returnValue = '');
+        }
+      };
+      window.addEventListener('beforeunload', listener);
+      return () => {
+        window.removeEventListener('beforeunload', listener);
+      };
+    }
+  }, [hasHandler]);
 };
 
 export default useBeforeunload;
