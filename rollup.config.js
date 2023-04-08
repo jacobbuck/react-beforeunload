7275064a956cb40142d477d514572eb032d4dece
Jacob Buck <buck.jacob@gmail.com>
2023-04-08 03:49:07 +0000

    Remove dependencies: - prop-types - tiny-invariant Remove dev-dependencies: - babel-plugin-dev-expression - babel-plugin-transform-react-remove-prop-types

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,11 @@
 import babel from '@rollup/plugin-babel';
-import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
   output: [
-    { file: pkg.main, format: 'cjs', sourcemap: true },
-    { file: pkg.module, format: 'esm', sourcemap: true },
+    { file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true },
+    { file: 'lib/index.esm.js', format: 'esm', sourcemap: true },
   ],
-  external: [
-    ...Object.keys(pkg.dependencies),
-    ...Object.keys(pkg.peerDependencies),
-  ],
+  external: ['react'],
   plugins: [babel({ babelHelpers: 'bundled' })],
 };
--- src/Beforeunload.js
+++ src/Beforeunload.js
@@ -1,12 +1,6 @@
-import PropTypes from 'prop-types';
 import { useBeforeunload } from './useBeforeunload';
 
 export const Beforeunload = ({ children = null, onBeforeunload }) => {
   useBeforeunload(onBeforeunload);
   return children;
-};
-
-Beforeunload.propTypes /* remove-proptypes */ = {
-  children: PropTypes.any,
-  onBeforeunload: PropTypes.func.isRequired,
 };
